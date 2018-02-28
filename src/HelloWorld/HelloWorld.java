45825596069a2bebd7e6a5f00313a7ebf5667c7a
widea9928 <widea9928@7HGM6Y1.Student.UGDSB.ED>
2018-02-28 13:23:06 -0500

    First commit

---
+++ src/HelloWorld/HelloWorld.java
@@ -0,0 +1,15 @@
+/*
+ * Created by Political_Cassandra
+ * 
+ * 
+ */
+
+package HelloWorld;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("hello"+" "+"world!");
+  }
+}
+
